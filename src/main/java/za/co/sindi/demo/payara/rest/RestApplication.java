6744d2e788242bc7c9687b2e7e027d0e3b73cee1
Buhake Sindi <buhake.sindi@sindi.co.za>
2020-02-12 15:58:22 +0200

    First Commit.

---
+++ src/main/java/za/co/sindi/demo/payara/rest/RestApplication.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package za.co.sindi.demo.payara.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * @author buhake.sindi
+ * @since 2018/08/01
+ *
+ */
+@ApplicationPath("/api")
+public class RestApplication extends Application {
+
+}
